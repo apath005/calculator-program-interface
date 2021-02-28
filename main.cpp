20163cb47ac12a78ccd049233f2dec1428321642
apath005@ucr.edu <apath005@hammer.cs.ucr.edu>
2021-02-28 11:56:41 -0800

    Implement factory and added functionality to main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "add.hpp"
+#include "sub.hpp"
+#include "div.hpp"
+#include "op.hpp"
+#include "pow.hpp"
+#include "mult.hpp"
+#include "factory.hpp"
+#include "base.hpp"
+#include <string>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char**argv) {
+	Factory* expressionTree = new Factory();
+        for(unsigned i = 1; i < argc; i++) {
+		cout << argv[i] << endl;
+	}
+	delete expressionTree;
+
+	Base* result = expressionTree->parse(argv, argc);
+	if(!result) {
+		cout << "ERROR: invalid expression" << endl;
+	} else {
+		cout << result->stringify() << " = " << result->evaluate() << endl;
+	}
+	delete result;
+	return 0;
+};
