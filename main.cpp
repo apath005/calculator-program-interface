f45cdbee3229e5f3e4f02d63117bb49878709772
apath005@ucr.edu <apath005@hammer.cs.ucr.edu>
2021-02-28 12:30:48 -0800

    Fixed typos

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char**argv) {
-	Factory* expressionTree = new Factory();
+	Factory* expression = new Factory();
         for(unsigned i = 1; i < argc; i++) {
 		cout << argv[i] << endl;
 	}
-	delete expressionTree;
+	delete expression;
 
-	Base* result = expressionTree->parse(argv, argc);
+	Base* result = expression->parse(argv, argc);
 	if(!result) {
 		cout << "ERROR: invalid expression" << endl;
 	} else {
